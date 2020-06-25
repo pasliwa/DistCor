dced89becfff9c99e1267635762079d6723909cd
Piotrek Sliwa <piotr.a.sliwa@gmail.com>
2020-06-25 03:14:20 +0200

    Add network creation from dictionary

--- generate_network.py
+++ generate_network.py
@@ -31,10 +31,38 @@ def create_network_from_file(file_name):
             end_node, expression = dep.split(" = ")
             start_nodes = [variable_name for variable_name in variable_names if variable_name in expression]
             edges_for_dep = list(itertools.product(start_nodes, end_node))
-            # augmentation = " = " + dep.split(" = ")[0]  + " + "
             df[dep.split(" = ")[0]] += df.eval(dep.split(" = ")[1])
-            # print(dep.replace(" = ", augmentation))
             edges.extend(edges_for_dep)
 
     Bayesian_network = CausalGraphicalModel(nodes=variable_names, edges=edges)
+    return df, Bayesian_network, noise_and_generating
+
+
+def create_network_from_dict(network_spec):
+    """
+    Network_spec partial description:
+    variable_names - list
+    generating_functions - list of triples
+    dependencies - list of pairs
+    """
+    random.seed(network_spec["random_seed"])
+    np.random.seed(network_spec["random_seed"])
+
+    df = pd.DataFrame(np.random.normal(network_spec["noise_mean"], network_spec["variance"],
+                                       size=(network_spec["nsamp"], len(network_spec["variable_names"]))),
+                      columns=network_spec["variable_names"])
+
+    number_of_generating_functions = int(f.readline().strip().split("number_of_generating_functions: ")[1])
+    for var_name, min_v, max_v in network_spec["generating_functions"]:
+        df[var_name] = np.random.uniform(float(min_v), float(max_v), network_spec["nsamp"])
+
+    edges = []
+    noise_and_generating = df.copy()
+    for end_node, expression in network_spec["dependencies"]:
+        start_nodes = [variable_name for variable_name in network_spec["variable_names"] if variable_name in expression]
+        edges_for_dep = list(itertools.product(start_nodes, end_node))
+        df[end_node] += df.eval(expression)
+        edges.extend(edges_for_dep)
+    edges = list(set(edges))
+    Bayesian_network = CausalGraphicalModel(nodes=network_spec["variable_names"], edges=edges)
     return df, Bayesian_network, noise_and_generating
