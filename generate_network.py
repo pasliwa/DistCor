3a65f99a56fe812b97a754838e267b1766e6b111
Piotrek Sliwa <piotr.a.sliwa@gmail.com>
2020-07-13 02:33:42 +0200

    Add new data creation sequence (clean data, noise, graph) and options to create from dict

--- generate_network.py
+++ generate_network.py
@@ -52,7 +52,7 @@ def create_network_from_dict(network_spec):
                                        size=(network_spec["nsamp"], len(network_spec["variable_names"]))),
                       columns=network_spec["variable_names"])
 
-    number_of_generating_functions = int(f.readline().strip().split("number_of_generating_functions: ")[1])
+    number_of_generating_functions = int(network_spec["number_of_generating_functions"])
     for var_name, min_v, max_v in network_spec["generating_functions"]:
         df[var_name] = np.random.uniform(float(min_v), float(max_v), network_spec["nsamp"])
 
@@ -66,3 +66,60 @@ def create_network_from_dict(network_spec):
     edges = list(set(edges))
     Bayesian_network = CausalGraphicalModel(nodes=network_spec["variable_names"], edges=edges)
     return df, Bayesian_network, noise_and_generating
+
+
+def create_clean_data(network_spec):
+    """
+    Network_spec partial description:
+    variable_names - list
+    generating_functions - list of triples
+    dependencies - list of pairs
+    """
+    random.seed(network_spec["random_seed"])
+    np.random.seed(network_spec["random_seed"])
+
+    df = pd.DataFrame(np.zeros(shape=(network_spec["nsamp"], len(network_spec["variable_names"]))),
+                      columns=network_spec["variable_names"])
+
+    for var_name, min_v, max_v in network_spec["generating_functions"]:
+        df[var_name] = np.random.uniform(float(min_v), float(max_v), network_spec["nsamp"])
+
+    for end_node, expression in network_spec["dependencies"]:
+        start_nodes = [variable_name for variable_name in network_spec["variable_names"] if variable_name in expression]
+        df[end_node] += df.eval(expression)
+    return df
+
+def add_noise(df, noise_mean, stdeviation):
+    return df + np.random.normal(noise_mean, stdeviation,
+            size=(df.shape))
+
+def from_file_to_network_spec(file_name):
+    with open(file_name) as f:
+        random_seed = f.readline().strip().split("random_seed: ")[1]
+        random_seed = int(random_seed) if random_seed != "no_seed" else 42
+
+        variable_names = f.readline().strip().split("variable_names: ")[1].split(" ")
+        nsamp = int(f.readline().strip().split("number_of_samples: ")[1])
+        noise_mean, noise_std = map(float, f.readline().strip().split("normal_noise: ")[1].split(" "))
+
+        number_of_generating_functions = int(f.readline().strip().split("number_of_generating_functions: ")[1])
+        generating_functions = []
+        for i in range(number_of_generating_functions):
+            var_name, min_v, max_v = f.readline().strip().split(" ")
+            generating_functions.append((var_name, min_v, max_v))
+        rest_of_dependencies = map(str.strip, f.readlines())
+        dependencies = []
+        for dep in rest_of_dependencies:
+            end_node, expression = dep.split(" = ")
+            dependencies.append((end_node, expression))
+        return {"random_seed": random_seed, "variable_names": variable_names, "nsamp": nsamp,
+        "noise_mean":noise_mean, "noise_std": noise_std, "generating_functions": generating_functions, "dependencies":dependencies}
+
+def get_graphical_network_model_from_dict(network_spec):
+    edges = []
+    for end_node, expression in network_spec["dependencies"]:
+        start_nodes = [variable_name for variable_name in network_spec["variable_names"] if variable_name in expression]
+        edges_for_dep = list(itertools.product(start_nodes, end_node))
+        edges.extend(edges_for_dep)
+        edges = list(set(edges))
+    return CausalGraphicalModel(nodes=network_spec["variable_names"], edges=edges)
